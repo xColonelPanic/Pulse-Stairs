a931605d09e9043ec52eb3449d120c6c30519951
xColonelPanic <agadboi2@illinois.edu>
2015-02-19 01:43:37 -0600

    minor big fix

--- presets.py
+++ presets.py
@@ -45,10 +45,14 @@ def transition_rgb(stair_range, rgb1, rgb2, duration):
 
 def spring_pulse(r, g, b):
 	for i in range(2,16):
+		if i > 2:
+			helpers.set_stair_rgb(i-2,0,0,0)
 		helpers.set_stair_rgb(i-1, r, g, b)
 		helpers.set_stair_rgb(i, r, g, b)
 		helpers.set_stair_rgb(i+1, r, g, b)
 	for i in range(2,16):
+		if i > 2:
+			helpers.set_stair_rgb(17-(i-2),0,0,0)
 		helpers.set_stair_rgb(17-(i-1), r, g, b)
 		helpers.set_stair_rgb(17-i, r, g, b)
 		helpers.set_stair_rgb(17-(i+1), r, g, b)
