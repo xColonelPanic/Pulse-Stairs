1b35570f8f5392e2447c7e8f8c198076dc4733dc
xColonelPanic <agadboi2@illinois.edu>
2015-02-19 01:54:54 -0600

    bug fix

--- presets.py
+++ presets.py
@@ -58,12 +58,13 @@ def spring_pulse(r, g, b):
 		helpers.set_stair_rgb(17-(i+1), r, g, b)
 
 def treadmill(r, g, b):
-	for i in range(-1,18):
+	for i in range(-1,19):
 		if i > 2:
 			helpers.set_stair_rgb(i-2,0,0,0)
 		helpers.set_stair_rgb(i-1, r, g, b)
 		helpers.set_stair_rgb(i, r, g, b)
 		helpers.set_stair_rgb(i+1, r, g, b)
+		time.sleep(0.5)
 
 def calibrate(x):
 		helpers.set_stair_rgb(x,255,0,0)
